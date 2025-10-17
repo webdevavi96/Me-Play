351bc6a188220c3f9286f4e4edf3c214aa7d6e78
webdevavi96 <avinashchaurasiya902@gmail.com>
2025-10-17 13:52:34 +0530

    Add watch history aggregation: implement getWatchHistory function to retrieve user's watched videos with owner details

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // Generating Access and Refresh tokens ->
 const generateAccesssAndRefreshToken = async (userId) => {
@@ -321,6 +322,57 @@ const getUserChannelList = asyncHandler(async (req, res) => {
 });
 
 
+// Pipelines for watch history
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user[0].watchHistory, "Watch histroy fetched successfully"));
+
+});
+
+
 export {
     registerUser,
     LogInUser,
@@ -331,5 +383,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUsercoverImage,
-    getUserChannelList
+    getUserChannelList,
+    getWatchHistory
 }
